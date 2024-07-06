ecb6c19505cf473489c0c50db643ad038608c20d
DimpleChandran <155473604+DimpleChandran@users.noreply.github.com>
2024-07-06 14:11:23 +0530

    Add files via upload

---
+++ src/components/Chatbot/ChatbotLeft.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ChatbotLeft.css'
+
+export default function ChatbotLeft() {
+    return ( <
+        div className = 'chatbot-content' >
+        <
+        h1 className = 'chatbot-heading' > CHAT WITH TRAVELBOT < /h1> <
+        p className = 'chatbot-paragraph' > Welcome user, please be patient and
+        let TRAVELBOT know your requirements! < /p> <
+            /div>
+    )
+}
---
+++ src/components/Header/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Logo.css'
+
+export default function Logo() {
+    return ( <
+        div >
+        <
+        h1 className = 'logo-heading' > TRAVELBOT < /h1> <
+        /div>
+    )
+}
